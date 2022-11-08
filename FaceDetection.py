06325a272416c325fe99ca134aa56e54cf670c94
Abhay Chowdhry <abhay21508@iiitd.ac.in>
2022-11-08 20:08:49 +0530

    Added eye detection

--- FaceDetection.py
+++ FaceDetection.py
@@ -2,6 +2,7 @@ import cv2
 import imutils
 
 face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_detector = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 vs = cv2.VideoCapture(0)
 
@@ -12,6 +13,12 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
+        eyes = eye_detector.detectMultiScale(gray, 1.3, 5)
+        print(eyes)
+        print(type(eyes))
+        for elemnts in eyes:
+            (ex, ey, ew, eh) = elemnts
+            cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow('img',img)
     key = cv2.waitKey(1)
